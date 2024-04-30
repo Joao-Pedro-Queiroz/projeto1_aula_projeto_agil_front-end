006f2622dc2ad30ec885c7fa6d75ee43253e5227
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-04-30 10:21:29 -0300

    pequenas alterações no recuperar senha

--- recuperar_senha.py
+++ recuperar_senha.py
@@ -2,26 +2,25 @@ import streamlit as st
 import requests
 
 # Configuração da URL base
-URL_BASE = "http://127.0.0.1:5000/usuarios"
+URL_BASE = "http://127.0.0.1:5000/senha"
 
 def recuperar_senha(email):
-    # Envia uma solicitação POST para o servidor Flask e retorna a resposta.
-    dados = {"email": email}
-    response = requests.post(URL_BASE, json=dados)
-    return response.json(), response.status_code
+    URL_BASE += f"?email={email}"
+    response = requests.get(URL_BASE,)
+    return response.json()[0], response.status_code
 
 def enviar_pedido_de_senha():
     # Valida o e-mail e envia o pedido de recuperação de senha.
     email = st.text_input("Digite seu e-mail para recuperar a senha:")
     if st.button("Enviar"):
         if email:
-            resposta, status_code = recuperar_senha(email)
+            usuario, status_code = recuperar_senha(email)
             if status_code == 200:
-                st.success(resposta.get("mensagem", "E-mail enviado com sucesso!"))
+                st.success(f"Sua senha é: {usuario['password']}")
             elif status_code == 404:
                 st.error("E-mail não encontrado.")
             elif status_code == 400:
-                st.error(resposta.get("erro", "Nenhum e-mail fornecido"))
+                st.error("Nenhum e-mail fornecido")
             else:
                 st.error("Ocorreu um erro ao enviar o e-mail.")
         else:
